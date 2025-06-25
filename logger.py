ed27ba453ddc34a8b59e8d789c351b8519652554
Carlos <valhallalima@gmail.com>
2025-06-25 01:23:44 -0300

    flush  print

--- logger.py
+++ logger.py
@@ -6,6 +6,6 @@ BR_TZ = timezone(timedelta(hours=-3))
 def log(msg):
     now = datetime.now(BR_TZ).strftime('%d/%m/%Y %H:%M:%S')
     full_msg = f"[{now}] {msg}"
-    print(full_msg)
+    print(full_msg,flush=True)
     with open("travian_bot.log", "a", encoding="utf-8") as f:
         f.write(full_msg + "\n")
